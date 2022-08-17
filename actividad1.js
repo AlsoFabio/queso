3e5282660f9770f2139f0897645762fa98c1049f
AlsoFabio <minette_sas@hotmail.com>
2022-08-17 13:04:17 -0300

    se agrego el archivo actividad1 por primera vez

---
+++ actividad1.js
@@ -0,0 +1,9 @@
+const miArray=["Lucas","Matias","Jose","Pedro","Martina","Marcelo","Esteban","Marcela","Martin"];
+console.log(`Cantidad de invitados: ${miArray.length}`)
+console.log(`El ultimo invitado es ${miArray[miArray.length-1]}`)
+miArray.splice(miArray.indexOf("Pedro"),1);
+miArray.push("Pedro");
+miArray.splice(miArray.indexOf("Marcela"),1);
+
+console.log(miArray.map(e => {return `${e}. Orden: ${miArray.indexOf(e)+1} `}))
+console.log(miArray.sort())
