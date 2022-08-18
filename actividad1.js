682d7e16fbca03e54f4d3f0b0ab9c6a5ce588ecf
AlsoFabio <minette_sas@hotmail.com>
2022-08-18 14:50:54 -0300

    se modifico el archivo por primera vez 18/07/2022

--- actividad1.js
+++ actividad1.js
@@ -4,6 +4,5 @@ console.log(`El ultimo invitado es ${miArray[miArray.length-1]}`)
 miArray.splice(miArray.indexOf("Pedro"),1);
 miArray.push("Pedro");
 miArray.splice(miArray.indexOf("Marcela"),1);
-
-console.log(miArray.map(e => {return `${e}. Orden: ${miArray.indexOf(e)+1} `}))
 console.log(miArray.sort())
+console.log(miArray.map(e => {return `${e}. Orden: ${miArray.indexOf(e)+1} `}))
